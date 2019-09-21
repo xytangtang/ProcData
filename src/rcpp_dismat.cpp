6267f5128e22539a5ca2a5320f821faa72fa9db2
zhiwpku <zhiwpku@gmail.com>
2019-09-20 20:06:23 -0400

    ProcData

--- src/rcpp_dismat.cpp
+++ src/rcpp_dismat.cpp
@@ -1,7 +1,17 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-int sum_abs_diff(IntegerVector x, IntegerVector y) {
+double sum_abs_diff(NumericVector x, NumericVector y) {
+  int l = std::min(x.length(), y.length());
+  double res = 0;
+  for(int i=0; i<l; i++){
+    int diff = x[i] - y[i];
+    res += diff > 0? diff : -diff;
+  }
+  return res;
+}
+
+int sum_abs_diff_int(IntegerVector x, IntegerVector y) {
   int l = std::min(x.length(), y.length());
   int res = 0;
   for(int i=0; i<l; i++){
@@ -30,7 +40,7 @@ double calculate_f1_cpp(StringVector seq1, StringVector seq2) {
   for(int i=0; i<n_common_events; i++){
     String event = common_events[i];
     int l = std::min(dict1[event].length(), dict2[event].length());
-    f_score += sum_abs_diff(dict1[event], dict2[event]);
+    f_score += sum_abs_diff_int(dict1[event], dict2[event]);
   }
   return f_score/std::max(seq1.length(), seq2.length());
 }
@@ -38,6 +48,22 @@ double calculate_f1_cpp(StringVector seq1, StringVector seq2) {
 int calculate_g_cpp(StringVector seq1, StringVector seq2) {
   return setdiff(seq1, seq2).length() + setdiff(seq2, seq1).length();
 }
+
+
+double calculate_time_f1_cpp(StringVector seq1, StringVector seq2, NumericVector ts1, NumericVector ts2) {
+  StringVector common_events = intersect(seq1, seq2);
+  int n_common_events = common_events.length();
+  double f_score = 0;
+  std::map<String, IntegerVector> dict1 = count_events(seq1);
+  std::map<String, IntegerVector> dict2 = count_events(seq2);
+  for(int i=0; i<n_common_events; i++){
+    String event = common_events[i];
+    int l = std::min(dict1[event].length(), dict2[event].length());
+    f_score += sum_abs_diff(ts1[dict1[event]], ts2[dict2[event]]);
+  }
+  return f_score/std::max(seq1.length(), seq2.length());
+}
+
 
 // [[Rcpp::export]]
 double calculate_dissimilarity_cpp(StringVector seq1, StringVector seq2) {
